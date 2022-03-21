0c1fe90c5e0fddbb7a46156f6fd8863fce23411f
ycanaan <yaelcanaan@gmail.com>
2022-03-21 11:19:44 -0400

    3/21

--- public/main.js
+++ public/main.js
@@ -58,11 +58,58 @@ function generateRandomAge() {
 
 function onLoad() {
 
-  let animal = generateRandomAnimal();
-  console.log(animal)
-  document.getElementById("animal-properties").textContent = animal.name + "  " + animal.age + "years old";
-  let imageTag = document.getElementById("animal-img");
-  imageTag.setAttribute("src", animal.image);
-  imageTag.setAttribute("alt", animal.image_alt);
+// get the savedAnimal in local storage if one exists
+  var animal = JSON.parse(localStorage.getItem("savedAnimal"));
 
-}
+  //use a boolean to keep track of whether you have saved an animal
+  var hasSavedAnimal = false;
+
+  //check if the saved animal exists in local storage
+  if (animal === null)
+  {
+    //if there is no saved animal, the button should display the Save Animal text
+    document.getElementById("button-storage").textContent = "Save Animal";
+
+    //if there is no saved animal, we generate one
+    animal = generateRandomAnimal();
+  }
+  else
+  {
+    //if there is a saved animal, the button should display Clear Animal text
+    document.getElementById("button-storage").textContent = "Clear Animal";
+
+    //change the boolean to note that this animal has been saved
+    hasSavedAnimal = true;
+  }
+
+  // update the page based on the animal properties
+  document.getElementById("animal-properties").textContent = animal.name + "  " + animal.age + "years old";
+  document.getElementById("animal-img").setAttribute("src", animal.image);
+
+
+  document.getElementById("button-storage").addEventListener("click", function() {
+    //when we are clearing the animal
+    if (hasSavedAnimal)
+    {
+      // clear the animal from the local storage
+      localStorage.removeItem("savedAnimal");
+
+      // if this button was clicked, hide button and show message to user
+      document.getElementById("button-storage").style.display = "none";
+      document.getElementById("button-action-text").textContent = "Cleared!";
+      document.getElementById("button-action-text").style.display = "block";
+    }
+    //when we are saving the animal
+    else
+    {
+      // save the animal to the local storage
+      localStorage.setItem("savedAnimal", JSON.stringify(animal));
+
+      // if this button was clicked, hide button and show message to user
+      document.getElementById("button-storage").style.display = "none";
+      document.getElementById("button-action-text").textContent = "Saved!";
+      document.getElementById("button-action-text").style.display = "block";
+    }
+  });
+
+};
